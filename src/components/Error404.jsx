7e45039c9590d080fa1aa30fe9a018efa9bdb11e
Tracy Reith <treith@treith.com>
2019-04-23 11:00:20 -0700

    Create queue

---
+++ src/components/Error404.jsx
@@ -0,0 +1,24 @@
+import React, { Component } from 'react'
+import { Link } from 'react-router-dom'
+import PropTypes from 'prop-types'
+
+function Error404(props) {
+
+  const Error404 =  {
+    background: '#fff',
+  }
+
+
+  return (
+    <div style={Error404}>
+      <h3>Page {props.location.pathname} not found</h3>
+      <p><Link to='/'>Back to the start</Link></p>
+    </div>
+  )
+}
+
+Error404.propTypes = {
+  location: PropTypes.object
+}
+
+export default Error404
--- src/components/NewTicketForm.js
+++ src/components/NewTicketForm.js
@@ -1,24 +1,80 @@
-import React from 'react'
+import React from 'react';
+import  PropTypes from 'prop-types';
+import { v4 } from 'uuid';
 
-function NewTicketForm(){
+
+function NewTicketForm(props){
+  let _names = null
+  let _location = null
+  let _issue = null
+
+  function handleNewTicketFormSubmission(event) {
+    event.preventDefault();
+    props.onNewTicketCreation({names: _names.value, location: _location.value, issue: _issue.value, id: v4()});
+    alert('Issue submitted.');
+    _names.value = '';
+    _location.value = '';
+    _issue.value = '';
+  }
+
   return (
     <div>
-      <form>
+      <style jsx>{`
+        form {
+          display: flex;
+          flex-flow: column nowrap;
+          justify-content: flex-start;
+          font-size: 1rem;
+        }
+
+        input, button, textarea {
+          margin-top: .5rem;
+        }
+
+        input {
+          width: 40vw;
+          font-size: .85rem;
+        }
+
+        textarea {
+          width: 90vw;
+          height: 100px;
+          font-size: .85rem;
+        }
+
+        button {
+          width: 100px;
+          self-align: flex-start;
+          background-color: #acceff;
+          border-radius: 4px;
+          text-transform: uppercase;
+          font-size: 1rem;
+        }
+      `}
+      </style>
+      <form onSubmit={handleNewTicketFormSubmission}>
         <input
           type='text'
           id='names'
-          placeholder='Pair Names'/>
+          placeholder='Pair Names'
+          ref={(input) => {_names = input}}/>
         <input
           type='text'
           id='location'
-          placeholder='Location'/>
+          placeholder='Location'
+          ref={(input) => {_location = input}}/>
         <textarea
           id='issue'
-          placeholder='Describe your issue.'/>
+          placeholder='Describe your issue.'
+          ref={(textarea) => {_issue = textarea}}/>
         <button type='submit'>Help!</button>
       </form>
     </div>
   )
 }
 
-export default NewTicketForm
+NewTicketForm.propTypes = {
+  onNewTicketCreation: PropTypes.func
+}
+
+export default NewTicketForm;
